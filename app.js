a5f781b4053f1022984b46f13ddb328bd0542342
tuanpham <pvtuanhcmus@gmail.com>
2017-10-10 17:43:43 +0700

    push code demo

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const bucketlist = require('./controllers/bucketlist');
+
+//Initialize our app variable
+const app = express();
+
+//Declaring Port
+const port = 3000;
+
+//Middleware for CORS
+app.use(cors());
+
+//Middleware for bodyparsing using both json and urlencoding
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+mongoose.connect(config.database);
+
+app.get('/', (req,res) => {
+    res.send("Invalid page e");
+});
+app.use('/bucketlist',bucketlist);
+//Listen to port 3000
+app.listen(port, () => {
+    console.log(`Starting the server at port ${port}`);
+});
